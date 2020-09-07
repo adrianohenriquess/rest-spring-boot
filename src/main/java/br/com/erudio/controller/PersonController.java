d01ee80a0b8b14f424659fb3b49b4898aa1714ca
adrianohenriquess <adriano.henriquesds@gmail.com>
2020-09-07 16:13:17 -0300

    Painação de dados com pageable e Hateoas para mostrar na lista todas as paginas anteriores e posteriores

--- src/main/java/br/com/erudio/controller/PersonController.java
+++ src/main/java/br/com/erudio/controller/PersonController.java
@@ -3,9 +3,15 @@ package br.com.erudio.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.data.vo.v1.PersonVO;
@@ -30,14 +37,37 @@ public class PersonController {
 	
 	@Autowired
 	private PersonServices service;
+	
+	@Autowired
+	private PagedResourcesAssembler<PersonVO> assembler;
 
 	//@CrossOrigin(origins = "http://localhost:8080")
 	@ApiOperation("Find All People")
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public List<PersonVO> findAll() {
-		List<PersonVO> list = service.findAll();
+	public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+									@RequestParam(value = "limit", defaultValue = "10") int limit,
+									@RequestParam(value = "order", defaultValue = "asc") String order) {
+		Direction sort = order.equalsIgnoreCase("desc") ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sort, "firstName"));
+		Page<PersonVO> list = service.findAll(pageable);
 		list.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-		return list;
+		PagedResources<?> resources = assembler.toResource(list);
+		return new ResponseEntity<>(resources, HttpStatus.OK);
+	}
+	
+	@ApiOperation("Find All People")
+	@GetMapping(value = "/findPersonByName/{firstName}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public ResponseEntity<?> findPersonByName(
+									@PathVariable("firstName") String firstName,
+									@RequestParam(value = "page", defaultValue = "0") int page,
+									@RequestParam(value = "limit", defaultValue = "10") int limit,
+									@RequestParam(value = "order", defaultValue = "asc") String order) {
+		Direction sort = order.equalsIgnoreCase("desc") ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sort, "firstName"));
+		Page<PersonVO> list = service.findPersonByName(firstName, pageable);
+		list.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		PagedResources<?> resources = assembler.toResource(list);
+		return new ResponseEntity<>(resources, HttpStatus.OK);
 	}	
 	
 	//@CrossOrigin(origins = {"http://localhost:8080", "www.erudio.com.br"})
--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,8 +1,8 @@
 package br.com.erudio.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,9 +24,19 @@ public class PersonServices {
 		return vo;
 	}
 	
-	public List<PersonVO> findAll() {
-		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
-	}	
+	public Page<PersonVO> findAll(Pageable pageable) {
+		Page<Person> page = repository.findAll(pageable);
+		return page.map(this::converToPersonVO);
+	}
+	
+	public Page<PersonVO> findPersonByName(String firstName, Pageable pageable) {
+		Page<Person> page = repository.findPersonByName(firstName, pageable);
+		return page.map(this::converToPersonVO);
+	}
+	
+	private PersonVO converToPersonVO(Person person) {
+		return DozerConverter.parseObject(person, PersonVO.class);
+	}
 	
 	public PersonVO findById(Long id) {
 
